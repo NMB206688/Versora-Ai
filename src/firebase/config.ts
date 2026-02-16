4a27734908552232d1203a58b227afa0c61a56eb
MAMIDI NAVEEN <naveenmamidi6798@gmail.com>
2026-02-16 08:23:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4605087149-e1d83",
+  "appId": "1:991117038025:web:6c561d780d5478668e9818",
+  "apiKey": "AIzaSyB0DhU-PzNBHc6JFRzxzjbX6jKbVoo4pMA",
+  "authDomain": "studio-4605087149-e1d83.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "991117038025"
+};
